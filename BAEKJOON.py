2f734f138fdff44cac647b6289ccf9086cfdb5bd
originaltitle2000 <original.title.2000@gmail.com>
2026-01-05 11:08:06 +0900

    Update

--- BAEKJOON.py
+++ BAEKJOON.py
@@ -1,17 +1,47 @@
 
-print ("Hello World!")
+# # print ("Hello World!")
 
-A, B = map(int, input().split())
-print (A + B)
+# # A, B = map(int, input().split())
+# # print (A + B)
 
-A, B = map(int, input().split())
-print (A - B)
+# # A, B = map(int, input().split())
+# # print (A - B)
 
-A, B = map(int, input().split())
-print (A * B)
+# # A, B = map(int, input().split())
+# # print (A * B)
 
-A, B = map(int, input().split())
-print (A / B)
+# # A, B = map(int, input().split())
+# # print (A / B)
 
-A, B = map(int, input().split())
-print (A + B, A - B, A * B, A // B, A % B)
+# # A, B = map(int, input().split())
+# # print (A + B, A - B, A * B, A // B, A % B)
+
+# # id = input()
+# # print(id + "??!")
+
+# # year = int(input())
+# # print(year - 543)
+
+# # A, B, C = map(int, input().split())
+
+# # print((A + B) % C)
+# # print(((A % C) + (B % C)) % C)
+# # print((A * B) % C)
+# # print(((A % C) * (B % C)) % C)
+
+# A = int(input())
+# B = int(input())
+
+# print(A * (B % 10))
+# print(A * (B // 10 % 10))
+# print(A * (B // 100))
+# print(A * B)
+
+# A = int(input())
+# B = int(input())
+
+# print(A * (B % 10))
+# print(A * (B // 10 % 10))
+# print(A * (B // 100))
+# print(A * B)
+
