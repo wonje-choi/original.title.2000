60971e0088c0c32c416ab7c60bed061007917925
originaltitle2000 <original.title.2000@gmail.com>
2026-01-05 11:19:12 +0900

    Update

--- BAEKJOON.py
+++ BAEKJOON.py
@@ -1,50 +1,52 @@
 
-print ("Hello World!")
+# print ("Hello World!")
 
-A, B = map(int, input().split())
-print (A + B)
+# A, B = map(int, input().split())
+# print (A + B)
 
-A, B = map(int, input().split())
-print (A - B)
+# A, B = map(int, input().split())
+# print (A - B)
 
-A, B = map(int, input().split())
-print (A * B)
+# A, B = map(int, input().split())
+# print (A * B)
 
-A, B = map(int, input().split())
-print (A / B)
+# A, B = map(int, input().split())
+# print (A / B)
 
-A, B = map(int, input().split())
-print (A + B, A - B, A * B, A // B, A % B)
+# A, B = map(int, input().split())
+# print (A + B, A - B, A * B, A // B, A % B)
 
-id = input()
-print(id + "??!")
+# id = input()
+# print(id + "??!")
 
-year = int(input())
-print(year - 543)
+# year = int(input())
+# print(year - 543)
 
-A, B, C = map(int, input().split())
+# A, B, C = map(int, input().split())
+# print((A + B) % C)
+# print(((A % C) + (B % C)) % C)
+# print((A * B) % C)
+# print(((A % C) * (B % C)) % C)
 
-print((A + B) % C)
-print(((A % C) + (B % C)) % C)
-print((A * B) % C)
-print(((A % C) * (B % C)) % C)
+# A = int(input())
+# B = int(input())
+# print(A * (B % 10))
+# print(A * (B // 10 % 10))
+# print(A * (B // 100))
+# print(A * B)
 
-A = int(input())
-B = int(input())
+# A = int(input())
+# B = int(input())
+# print(A * (B % 10))
+# print(A * (B // 10 % 10))
+# print(A * (B // 100))
+# print(A * B)
 
-print(A * (B % 10))
-print(A * (B // 10 % 10))
-print(A * (B // 100))
-print(A * B)
+# A, B ,C = map(int, input().split())
+# print (A + B + C)
 
-A = int(input())
-B = int(input())
+# print("\\    /\\")
+# print(" )  ( ')")
+# print("(  /  )")
+# print(" \\(__)|")
 
-print(A * (B % 10))
-print(A * (B // 10 % 10))
-print(A * (B // 100))
-print(A * B)
-
-A, B ,C = map(int, input().split())
-
-print (A + B + C)
