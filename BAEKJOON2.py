0feb963ada438c57c96ce8e6e5f0d761566d36bd
originaltitle2000 <original.title.2000@gmail.com>
2026-01-06 11:09:56 +0900

    Update

--- BAEKJOON2.py
+++ BAEKJOON2.py
@@ -1,6 +1,23 @@
 
-A, B = map(int, input().split())
+# A, B = map(int, input().split())
 
-if A > B:print(">")
-elif A < B:print("<")
-else:print("==")
+# if A > B:
+#     print(">")
+# elif A < B:
+#     print("<")
+# else:
+#     print("==")
+
+# score = int(input())
+
+# if score >= 90:
+#     print("A")
+# elif score >= 80:
+#     print("B")
+# elif score >= 70:
+#     print("C")
+# elif score >= 60:
+#     print("D")
+# else:
+#     print("F")
+
