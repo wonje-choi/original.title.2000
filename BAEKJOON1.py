92bbff4b0a3b2ef5cf5cc8d0b7281564a70ca5f6
originaltitle2000 <original.title.2000@gmail.com>
2026-01-06 10:51:29 +0900

    Add BAEKJOON1 and BAEKJOON2

---
+++ BAEKJOON1.py
@@ -0,0 +1,57 @@
+
+print ("Hello World!")
+
+A, B = map(int, input().split())
+print (A + B)
+
+A, B = map(int, input().split())
+print (A - B)
+
+A, B = map(int, input().split())
+print (A * B)
+
+A, B = map(int, input().split())
+print (A / B)
+
+A, B = map(int, input().split())
+print (A + B, A - B, A * B, A // B, A % B)
+
+id = input()
+print(id + "??!")
+
+year = int(input())
+print(year - 543)
+
+A, B, C = map(int, input().split())
+print((A + B) % C)
+print(((A % C) + (B % C)) % C)
+print((A * B) % C)
+print(((A % C) * (B % C)) % C)
+
+A = int(input())
+B = int(input())
+print(A * (B % 10))
+print(A * (B // 10 % 10))
+print(A * (B // 100))
+print(A * B)
+
+A = int(input())
+B = int(input())
+print(A * (B % 10))
+print(A * (B // 10 % 10))
+print(A * (B // 100))
+print(A * B)
+
+A, B ,C = map(int, input().split())
+print (A + B + C)
+
+print("\\    /\\")
+print(" )  ( ')")
+print("(  /  )")
+print(" \\(__)|")
+
+print("|\\_/|")
+print("|q p|   /}")
+print("( 0 )\"\"\"\\")
+print("|\"^\"`    |")
+print("||_/=\\\\__|")
