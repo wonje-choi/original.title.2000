f07a5d53a904bed999b64cdb670ada6b5fb50a7a
originaltitle2000 <original.title.2000@gmail.com>
2026-01-07 11:16:17 +0900

    Update

--- BAEKJOON2.py
+++ BAEKJOON2.py
@@ -61,3 +61,16 @@ H = (total_minutes // 60) % 24
 M = total_minutes % 60
 
 print(H, M)
+
+a, b, c = map(int, input().split())
+
+if a == b == c:
+    print(10000 + a * 1000)
+elif a == b:
+    print(1000 + a * 100)
+elif b == c:
+    print(1000 + b * 100)
+elif a == c:
+    print(1000 + a * 100)
+else:
+    print(max(a, b, c) * 100)
