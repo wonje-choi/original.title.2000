e261acd52b506ff383f29e1feda8360ec6c91524
originaltitle2000 <original.title.2000@gmail.com>
2026-01-08 20:01:17 +0900

    Update

--- BAEKJOON3.py
+++ BAEKJOON3.py
@@ -1,12 +1,28 @@
 
-# N = int(input())
-# for i in range(1, 10):
-#     print(f"{N} * {i} = {N * i}")
+N = int(input())
+for i in range(1, 10):
+    print(f"{N} * {i} = {N * i}")
 
-# T = int(input())
-# for _ in range(T):
-#     A, B = map(int, input().split())
-#     print(A + B)
+T = int(input())
+for _ in range(T):
+    A, B = map(int, input().split())
+    print(A + B)
 
 n = int(input())
 print(n * (n + 1) // 2)
+
+
+X = int(input())
+N = int(input())
+
+total = 0
+for _ in range(N):
+    a, b = map(int, input().split())
+    total += a * b
+
+if total == X:
+    print("Yes")
+else:
+    print("No")
+
+    
