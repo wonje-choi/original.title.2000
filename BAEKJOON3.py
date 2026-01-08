eddc86415400a1869c18bf694796ca451ef8de22
originaltitle2000 <original.title.2000@gmail.com>
2026-01-08 19:55:13 +0900

    Update

--- BAEKJOON3.py
+++ BAEKJOON3.py
@@ -1,5 +1,10 @@
 
-N = int(input())
-for i in range(1, 10):
-    print(f"{N} * {i} = {N * i}")
+# N = int(input())
+# for i in range(1, 10):
+#     print(f"{N} * {i} = {N * i}")
+
+# T = int(input())
+# for _ in range(T):
+#     A, B = map(int, input().split())
+#     print(A + B)
 
