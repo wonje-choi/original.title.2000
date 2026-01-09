639da0957b5580703f4dd3392e805512bfce0e64
wonje-choi <wonje@example.com>
2026-01-09 19:56:42 +0900

    Update

--- BAEKJOON3.py
+++ BAEKJOON3.py
@@ -51,3 +51,7 @@ T = int(input())
 for i in range(1, T + 1):
     A, B = map(int, input().split())
     print(f"Case #{i}: {A} + {B} = {A + B}")
+
+N = int(input())
+for i in range(1, N + 1):
+    print('*' * i)
