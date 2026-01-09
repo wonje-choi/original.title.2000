640b320d006a35502cd13836d08a1390283af522
wonje-choi <wonje@example.com>
2026-01-09 19:58:29 +0900

    Update

--- BAEKJOON3.py
+++ BAEKJOON3.py
@@ -60,3 +60,8 @@ N = int(input())
 for i in range(1, N + 1):
     print(' ' * (N - i) + '*' * i)
 
+while True:
+    A, B = map(int, input().split())
+    if A == 0 and B == 0:
+        break
+    print(A + B)
