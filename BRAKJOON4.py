c586129a1319bfff49f0b8af369467aa05f28144
wonje-choi <original.title.2000@gmail.com>
2026-01-10 11:24:33 +0900

    Update

--- BRAKJOON4.py
+++ BRAKJOON4.py
@@ -29,3 +29,14 @@ max_index = numbers.index(max_value) + 1
 print(max_value)
 print(max_index)
 
+N, M = map(int, input().split())
+
+baskets = [0] * (N + 1)
+
+for _ in range(M):
+    i, j, k = map(int, input().split())
+    for basket in range(i, j + 1):
+        baskets[basket] = k
+
+print(' '.join(map(str, baskets[1:])))
+
