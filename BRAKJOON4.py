0ba9d365168fe02bbdeef3331795a1c5ab232f47
wonje-choi <original.title.2000@gmail.com>
2026-01-10 11:22:15 +0900

    Update

--- BRAKJOON4.py
+++ BRAKJOON4.py
@@ -19,3 +19,13 @@ numbers = list(map(int, input().split()))
 
 print(min(numbers), max(numbers))
 
+numbers = []
+for _ in range(9):
+    numbers.append(int(input()))
+
+max_value = max(numbers)
+max_index = numbers.index(max_value) + 1
+
+print(max_value)
+print(max_index)
+
