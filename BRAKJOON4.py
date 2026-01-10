f41e4de8f9e21854fe2b92dbf4c8da0d90954a54
wonje-choi <original.title.2000@gmail.com>
2026-01-10 11:17:57 +0900

    Update

--- BRAKJOON4.py
+++ BRAKJOON4.py
@@ -7,3 +7,9 @@ v = int(sys.stdin.readline())
 
 print(numbers.count(v))
 
+N, X = map(int, input().split())
+numbers = list(map(int, input().split()))
+
+for num in numbers:
+    if num < X:
+        print(num, end=' ')
