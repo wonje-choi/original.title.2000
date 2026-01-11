723814d9d3165b91d960791dbd76a51b0e257db8
wonje-choi <original.title.2000@gmail.com>
2026-01-11 14:34:54 +0900

    Update

--- BRAKJOON4.py
+++ BRAKJOON4.py
@@ -78,3 +78,10 @@ for _ in range(M):
 print(*basket)
 
 
+N = int(input())
+scores = list(map(int, input().split()))
+
+M = max(scores)
+
+new_scores = [score / M * 100 for score in scores]
+print(sum(new_scores) / N)
