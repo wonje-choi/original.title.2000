69785c8062527e7b1296d799c72628839d04739c
wonje-choi <original.title.2000@gmail.com>
2026-01-14 14:31:08 +0900

    Update

--- BAEKJOON6.py
+++ BAEKJOON6.py
@@ -1,14 +1,14 @@
 
-# print("         ,r'\"7")
-# print("r`-_   ,'  ,/")
-# print(" \\. \". L_r'")
-# print("   `~\\/")
-# print("     |")
-# print("     |")
+print("         ,r'\"7")
+print("r`-_   ,'  ,/")
+print(" \\. \". L_r'")
+print("   `~\\/")
+print("     |")
+print("     |")
 
 
-# a, b, c, d, e, f = map(int, input().split())
-# print(1-a, 1-b, 2-c, 2-d, 2-e, 8-f)
+a, b, c, d, e, f = map(int, input().split())
+print(1-a, 1-b, 2-c, 2-d, 2-e, 8-f)
 
 
 n = int(input())
@@ -18,3 +18,23 @@ for i in range(1, n + 1):
 
 for i in range(n - 1, 0, -1):
     print(' ' * (n - i) + '*' * (2 * i - 1))
+
+
+s = input()
+print(1 if s == s[::-1] else 0)
+
+
+s = input().upper()
+counts = [0] * 26
+
+for c in s:
+    counts[ord(c) - ord('A')] += 1
+
+max_count = max(counts)
+
+if counts.count(max_count) > 1:
+    print('?')
+else:
+    print(chr(counts.index(max_count) + ord('A')))
+
+
