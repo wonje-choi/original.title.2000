de071f01400269089e678276004ef7bc2e9b7468
wonje-choi <original.title.2000@gmail.com>
2026-01-15 09:56:06 +0900

    Update

--- BAEKJOON7.py
+++ BAEKJOON7.py
@@ -1,1 +1,10 @@
 
+n, m = map(int, input().split())
+
+A = [list(map(int, input().split())) for _ in range(n)]
+B = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(n):
+    print(' '.join(str(A[i][j] + B[i][j]) for j in range(m)))
+
+
