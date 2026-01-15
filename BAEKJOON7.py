5717d53ef85352371b79db80efd073345b4ea2d2
wonje-choi <original.title.2000@gmail.com>
2026-01-15 09:57:09 +0900

    Update

--- BAEKJOON7.py
+++ BAEKJOON7.py
@@ -8,3 +8,18 @@ for i in range(n):
     print(' '.join(str(A[i][j] + B[i][j]) for j in range(m)))
 
 
+max_val = -1
+max_row, max_col = 0, 0
+
+for i in range(9):
+    row = list(map(int, input().split()))
+    for j in range(9):
+        if row[j] > max_val:
+            max_val = row[j]
+            max_row = i + 1
+            max_col = j + 1
+
+print(max_val)
+print(max_row, max_col)
+
+
