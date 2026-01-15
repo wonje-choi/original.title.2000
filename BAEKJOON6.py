3ae807e0bd6acee9b278f12d8632e92dee65754d
wonje-choi <original.title.2000@gmail.com>
2026-01-15 09:47:30 +0900

    Update

--- BAEKJOON6.py
+++ BAEKJOON6.py
@@ -69,3 +69,15 @@ for _ in range(n):
         count += 1
 
 print(count)
+
+
+gp = {'A+':4.5,'A0':4.0,'B+':3.5,'B0':3.0,'C+':2.5,'C0':2.0,'D+':1.5,'D0':1.0,'F':0}
+tc = ts = 0
+
+for _ in range(20):
+    _, c, g = input().split()
+    if g != 'P':
+        tc += float(c)
+        ts += float(c) * gp[g]
+
+print(ts / tc)
