626590da863cf0a6d903e2d2bdc2f7984e28a0f3
wonje-choi <original.title.2000@gmail.com>
2026-01-16 10:06:02 +0900

    Update

--- BAEKJOON8.py
+++ BAEKJOON8.py
@@ -1,1 +1,16 @@
 
+n, b = input().split()
+b = int(b)
+
+result = 0
+for i, char in enumerate(reversed(n)):
+    if char.isdigit():
+        digit = int(char)
+    else:
+        digit = ord(char) - ord('A') + 10
+    
+    result += digit * (b ** i)
+
+print(result)
+
+
