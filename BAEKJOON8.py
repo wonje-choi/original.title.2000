a02d302f71de08d82301d52df2ae82768928b320
wonje-choi <original.title.2000@gmail.com>
2026-01-17 18:02:15 +0900

    Update

--- BAEKJOON8.py
+++ BAEKJOON8.py
@@ -14,3 +14,15 @@ for i, char in enumerate(reversed(n)):
 print(result)
 
 
+n, b = map(int, input().split())
+
+digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+result = ''
+while n > 0:
+    result = digits[n % b] + result
+    n //= b
+
+print(result)
+
+
