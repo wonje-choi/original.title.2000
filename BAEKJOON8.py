af775cdc07428318e19231e9379c5f7790fab4b1
wonje-choi <original.title.2000@gmail.com>
2026-01-17 18:03:11 +0900

    Update

--- BAEKJOON8.py
+++ BAEKJOON8.py
@@ -26,3 +26,21 @@ while n > 0:
 print(result)
 
 
+T = int(input())
+for _ in range(T):
+    C = int(input())
+    
+    quarter = C // 25
+    C %= 25
+    
+    dime = C // 10
+    C %= 10
+    
+    nickel = C // 5
+    C %= 5
+    
+    penny = C
+    
+    print(quarter, dime, nickel, penny)
+
+
