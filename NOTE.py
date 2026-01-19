92c9ae8f14a0f62e488373b4d0fb17d95498fc96
wonje-choi <original.title.2000@gmail.com>
2026-01-19 18:43:53 +0900

    Update

--- NOTE.py
+++ NOTE.py
@@ -1,9 +1,21 @@
 
-print('안녕하세요')
+# print('안녕하세요.') #인사 코드
 
-print('A')
-print('B')
-print('C')
+# print('A')
+# '''
+# 여러줄의
+# 주석을
+# 처리하는
+# 방법입니다.
+# '''
+# print('B')
+# print('C')
 
-print('hi~ Python')
+# print('hi~ Python')
 
+
+a = 10
+
+print(a)
+print(b)
+print('a')
