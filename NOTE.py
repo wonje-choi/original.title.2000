92d6c9894671ec0d5b40690c35dbeafa0afc9226
wonje-choi <original.title.2000@gmail.com>
2026-01-24 11:59:59 +0900

    Update

--- NOTE.py
+++ NOTE.py
@@ -219,9 +219,20 @@
 #     print ( '{}은(는) 양수입니다.' .format (num) )
 
 
-num = int ( input ('숫자하나 입력 : ') )
+# num = int ( input ('숫자하나 입력 : ') )
 
-if num % 2 == 0 :
-    print ( '{}은(는) 짝수입니다.' .format (num) )
-else :
-    print ( '{}은(는) 홀수입니다.' .format (num) )
+# if num % 2 == 0 :
+#     print ( '{}은(는) 짝수입니다.' .format (num) )
+# else :
+#     print ( '{}은(는) 홀수입니다.' .format (num) )
+
+
+age = int ( input ('나이 입력 : ') )
+
+if age <= 7 :
+    print ( '유아입니다')
+elif age <= 19 :
+    print ( '청소년입니다')
+elif age >= 20 :
+    print ( '성인입니다')
+
