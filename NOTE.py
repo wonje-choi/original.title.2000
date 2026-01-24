b891b6c2776383b8e924d6c8eb3ba1cb2d46125d
wonje-choi <original.title.2000@gmail.com>
2026-01-24 12:48:21 +0900

    Update

--- NOTE.py
+++ NOTE.py
@@ -227,12 +227,20 @@
 #     print ( '{}은(는) 홀수입니다.' .format (num) )
 
 
-age = int ( input ('나이 입력 : ') )
+# age = int ( input ('나이 입력 : ') )
 
-if age <= 7 :
-    print ( '유아입니다')
-elif age <= 19 :
-    print ( '청소년입니다')
-elif age >= 20 :
-    print ( '성인입니다')
+# if age <= 7 :
+#     print ( '유아입니다')
+# elif age <= 19 :
+#     print ( '청소년입니다')
+# elif age >= 20 :
+#     print ( '성인입니다')
 
+text = input ( '알파벳 입력 :' )
+
+if text.isupper () : 
+    print ( '대문자' )
+elif text.islower () : 
+    print ( '소문자' )
+else : 
+    print ( '대/소문자 구분 불가능' )
