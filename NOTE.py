8a8503d430cac9783d58ed8fb987e7c38fefdf45
wonje-choi <original.title.2000@gmail.com>
2026-01-26 19:47:08 +0900

    Update

--- NOTE.py
+++ NOTE.py
@@ -264,14 +264,22 @@
 #     print ( n )
 #     n = n - 1
 
-n = 1
-while n <= 10:
-    print ( n )
-    n = n + 1
+# n = 1
+# while n <= 10:
+#     print ( n )
+#     n = n + 1
 
-text = 'yes'
+# text = 'yes'
 
-while text == 'yes':
-    text = input ( 'yes 입력 시 반복' )
+# while text == 'yes':
+#     text = input ( 'yes 입력 시 반복' )
+
+# print ( '종료' )
+
+
+text = input ( 'e 또는 E 입력시 종료' )
+
+while text != 'e' and text != 'E':
+    text = input ('e 또는 E 입력 시 종료')
 
 print ( '종료' )
