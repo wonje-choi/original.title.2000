9a0ad7daaaa48e71bf4fe93e61c30f54250ea3da
wonje-choi <original.title.2000@gmail.com>
2026-01-28 20:31:17 +0900

    Update

--- NOTE.py
+++ NOTE.py
@@ -317,23 +317,29 @@
 #     print ( i )
 
 
-for i in range (1, 11):
-    if i == 5:
-        continue
-    print (i)
+# for i in range (1, 11):
+#     if i == 5:
+#         continue
+#     print (i)
 
 
-for i in range (1, 11):
-    if i == 5:
-        break
-    print (i)
+# for i in range (1, 11):
+#     if i == 5:
+#         break
+#     print (i)
 
 
-for i in range (1, 11):
-    if i == 5:
-        pass
-    print (i)
+# for i in range (1, 11):
+#     if i == 5:
+#         pass
+#     print (i)
 
+n = int (input ('n:') )
+
+if n > 0:
+    pass
+else :
+    pass
 
 
 # age = int (input ( '나이 입력:' ) )
