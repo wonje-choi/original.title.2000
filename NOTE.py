225d6d90eba2ee30456f9b2827406b1178c4c85c
wonje-choi <original.title.2000@gmail.com>
2026-01-29 21:03:08 +0900

    Update

--- NOTE.py
+++ NOTE.py
@@ -392,9 +392,23 @@
 
 # li[2] = 'd'
 
-li = ['a', 'b', 'c', 'd', 'e']
+# li = ['a', 'b', 'c', 'd', 'e']
 
-li. index ('c')
+# li. index ('c')
 
-li. append ('f')
-li. insert (0, 'aa')
+# li. append ('f')
+# li. insert (0, 'aa')
+
+# li. remove ('aa')
+# del li [2]
+
+# 'b' in li
+
+# len (li)
+
+# li. count ('a')
+
+num = [1, 2, 3, 4, 5, 9, 7, 6, 8, 10]
+
+sun (num)
+min (num)
