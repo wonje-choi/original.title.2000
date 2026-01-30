72368b3f08fa5ae0ccaf4cb007d36bac7f229302
wonje-choi <original.title.2000@gmail.com>
2026-01-30 09:31:57 +0900

    Update

--- NOTE.py
+++ NOTE.py
@@ -420,12 +420,33 @@
 # num. sort (reverse = True)
 
 
-tu = ()
-tu = tupie ()
+# tu = ()
+# tu = tupie ()
+
+# tu = ('a', 'b', 'c')
+# tu [0]
+# tu [1]
+# tu [2]
+
+# tu [2] = 'd'
+
+# tu = ('a', 'b', 'c', 'a')
+
+# tu. index ('c')
+
+# 'b' in tu
+# len (tu)
+# tu. count ('a')
+
+num = (5, 7, 9)
+n1, n2 n3 = num
+
+a = 'hello'
+b = 'world'
+(a, b) = (b, a)
+
+li = ['a', 'b', 'c', 'd', 'e', 'f']
+tuple (li)
 
 tu = ('a', 'b', 'c')
-tu [0]
-tu [1]
-tu [2]
-
-tu [2] = 'd'
+list (tu)
