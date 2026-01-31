6e703da3ffc0d86a87fcca3e3214ac04c9883918
wonje-choi <original.title.2000@gmail.com>
2026-01-31 10:53:12 +0900

    Update

--- NOTE.py
+++ NOTE.py
@@ -479,13 +479,18 @@
 # a - b
 # a ^ b
 
-dic = {}
-dic = dict ()
+# dic = {}
+# dic = dict ()
 
-dic = {'kor':80, 'eng':90, 'mat':77}
-dic ['kor']
-dic ['kor'] = 85
-dic ['sci'] = 92
+# dic = {'kor':80, 'eng':90, 'mat':77}
+# dic ['kor']
+# dic ['kor'] = 85
+# dic ['sci'] = 92
 
-dic [0]
+# dic [0]
+
+del dic ['mat']
+dic. clear ()
+'eng' in dic
+len (dic)
 
