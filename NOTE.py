9d79fd54c1bb2691f5b6d36ca3820b8fb8106b60
wonje-choi <original.title.2000@gmail.com>
2026-01-31 10:51:31 +0900

    Update

--- NOTE.py
+++ NOTE.py
@@ -460,22 +460,32 @@
 
 # a = [0]
 
-a. add (5)
-a. remove (5)
-1 in a
-len (a)
-sum (num)
-min (num)
-max (num)
+# a. add (5)
+# a. remove (5)
+# 1 in a
+# len (a)
+# sum (a)
+# min (a)
+# max (a)
 
-list (a)
-tuple (a)
+# list (a)
+# tuple (a)
 
-a = {1, 2, 3}
-b = {2, 3, 4}
+# a = {1, 2, 3}
+# b = {2, 3, 4}
 
-a & b
-a | b
-a - b
-a ^ b
+# a & b
+# a | b
+# a - b
+# a ^ b
+
+dic = {}
+dic = dict ()
+
+dic = {'kor':80, 'eng':90, 'mat':77}
+dic ['kor']
+dic ['kor'] = 85
+dic ['sci'] = 92
+
+dic [0]
 
