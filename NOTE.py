45c70d96bbb4b8061c300d98a54fffb4bde1f806
wonje-choi <original.title.2000@gmail.com>
2026-02-01 20:21:58 +0900

    Update

--- NOTE.py
+++ NOTE.py
@@ -509,17 +509,26 @@
 # dict (li)
 
 
-li1 = intput ('문자 입력'). split ()
+# li1 = input ('문자 입력'). split ()
 
-li2 = list (input ('문자 입력'))
+# li2 = list (input ('문자 입력'))
 
-li3 = []
-li3. append (int (input ('숫자 입력')))
-li3. append (int (input ('숫자 입력')))
-li3. append (int (input ('숫자 입력')))
+# li3 = []
+# li3. append (int (input ('숫자 입력')))
+# li3. append (int (input ('숫자 입력')))
+# li3. append (int (input ('숫자 입력')))
 
-li4 = list (map (int, input ('숫자 입력'). split ()))
+# li4 = list (map (int, input ('숫자 입력'). split ()))
+# a = input ('숫자 입력'). split ()
+# b = map (int, a)
+# c = list (b)
 
-a = input ('숫자 입력'). split ()
-b = map (int, a)
-c = list (b)
+num = list (map (int, input ('숫자 입력'). split()))
+
+num. sort ()
+
+print ('합 :', sum (num))
+print ('평균 :', sum (num) / len (num))
+print ('최소값 :', num [0])
+print ('최대값 :', num [len (num) - 1])
+print ('중간값 :', num [len (num) //])
