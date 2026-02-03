1e5bd2005d9f87aaaf755457179f8d16064cf401
wonje-choi <original.title.2000@gmail.com>
2026-02-03 18:38:17 +0900

    Update

--- NOTE.py
+++ NOTE.py
@@ -562,8 +562,23 @@ for i in range (5):
     li. append (int (input ('숫자 입력')))
 
 for i in range (len (li)):
-    print (li[i])
+    print (li [i])
 
 for i in li:
     print (i)
+
+for i in range (len (li)):
+    if i % 2 == 0:
+        print (li [i])
+
+up = []
+low = []
+
+for i in li:
+    if i. isupper():
+        up. append (i)
+    elif i. islower ():
+        low. append (i)
+print (up)
+print (low)
 
