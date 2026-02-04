18d1724d46ae7a9ec225a193501f911cf24c2eec
wonje-choi <original.title.2000@gmail.com>
2026-02-04 22:52:25 +0900

    Update

--- NOTE.py
+++ NOTE.py
@@ -583,19 +583,22 @@
 # print (low)
 
 
-import math
-math. ceil (2.1)
-math. floor (2.1)
-math. factorial (10)
-math. sqrt (4)
-math. pi
+# import math
+# math. ceil (2.1)
+# math. floor (2.1)
+# math. factorial (10)
+# math. sqrt (4)
+# math. pi
 
-import random
-random. randint (1, 5)
-random. random ()
+# import random
+# random. randint (1, 5)
+# random. random ()
 
-li = ['a', 'b', 'c', 'd', 'e']
-random. choice (li)
-random. sample (li, 3)
-random. shuffle (li)
+# li = ['a', 'b', 'c', 'd', 'e']
+# random. choice (li)
+# random. sample (li, 3)
+# random. shuffle (li)
 
+n = (0)
+
+sum = 1, n
