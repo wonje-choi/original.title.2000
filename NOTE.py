3e1402f38098040b623b9aca2a1ef2fbcf289a84
wonje-choi <original.title.2000@gmail.com>
2026-02-04 23:01:53 +0900

    Update

--- NOTE.py
+++ NOTE.py
@@ -606,11 +606,20 @@
 #     s = s + i
 # print (s)
 
-n = int(input ('n:'))
-s = 1
+# n = int(input ('n:'))
+# s = 1
 
-for i in range (1, n + 1):
-    s = s * i
-print (s)
+# for i in range (1, n + 1):
+#     s = s * i
+# print (s)
 
+
+# n = int(input ('n:'))
+# s = 0
+
+# while n != 0:
+#     s = s + n
+#     n = int(input ('n:'))
+
+# print (s)
 
