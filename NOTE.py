690e00cb00ae463d5f33eccceff55a875469687d
wonje-choi <original.title.2000@gmail.com>
2026-02-08 19:56:56 +0900

    Update

--- NOTE.py
+++ NOTE.py
@@ -732,15 +732,35 @@
 # print (check)
 
 
-n = int (input ('n:'))
+# n = int (input ('n:'))
 
-check = 0
-for i in range (n, n + 1):
-    if n % i == 0:
-        check = check + 1
+# check = 0
+# for i in range (n, n + 1):
+#     if n % i == 0:
+#         check = check + 1
 
-if check == 2:
-    print (True)
-else :
-    print (False)
+# if check == 2:
+#     print (True)
+# else :
+#     print (False)
+
+# n = int (input ('n:'))
+# check = True
+# for i in range (2, n):
+#     if n % i == 0:
+#         check = False
+# print (check)
+
+
+# a = int (input ('a:'))
+
+# li = []
+
+# for n in range (2, a + 1):
+#     check = True
+#     for i in range (2, n):
+#         if n % i == 0:
+#             check = False
+#     if check :
+#         li .append (n)
 
