5879cbfa74236815cc2a08e838bd97a2196cba5e
wonje-choi <original.title.2000@gmail.com>
2026-02-15 11:03:26 +0900

    Update

--- NOTE.py
+++ NOTE.py
@@ -938,24 +938,23 @@
 #         break
 
 
-li = [8, 6, 4, 1, 2, 3, 5 , 10, 9, 7]
-
-for i in range (len (li)):
-    m_index = i
-    for j in range (i, len (li)):
-        if li [j] < li [m_index]:
-            m_index = j
-    li [i], li[m_index] = li [m_index], li [i]
-
-# print (li)
-
-
 # li = [8, 6, 4, 1, 2, 3, 5 , 10, 9, 7]
 
 # for i in range (len (li)):
-#     for j in range (len (li) - 1):
-#         if li [j] > li [j + 1]:
-#             li [j], li [j + 1] = li [j + 1], li [j]
+#     m_index = i
+#     for j in range (i, len (li)):
+#         if li [j] < li [m_index]:
+#             m_index = j
+#     li [i], li[m_index] = li [m_index], li [i]
 
 # print (li)
 
+
+li = [8, 6, 4, 1, 2, 3, 5 , 10, 9, 7]
+
+for i in range (len (li)):
+    for j in range (len (li) - 1):
+        if li [j] > li [j + 1]:
+            li [j], li [j + 1] = li [j + 1], li [j]
+
+print (li)
