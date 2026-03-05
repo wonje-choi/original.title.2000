ae072a1c8f985203e927efad9a22f86dbbcda72a
wonje-choi <original.title.2000@gmail.com>
2026-03-05 23:36:07 +0900

    메시지

--- C/C_NOTE.c
+++ C/C_NOTE.c
@@ -1,12 +1,23 @@
 
 #include <stdio.h>
 
-int main(void) {
+int main(){
     printf("Hello, C!\n");
     return 0;
     int a;
+    int b;
     a = 2;
+    b = 3;
     printf("%d\n", a);
-
+    printf("%d\n", b);
+    printf("%d\n", a + b);
+    printf("a: %d, b: %d, a + b: %d\n", a, b, a + b);
 }
 
+#include <stdio.h>
+
+main () {
+    char x;
+    x = 'A';
+    printf("%c\n", x);
+}
