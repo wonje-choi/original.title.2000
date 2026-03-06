926a7d3b84e256a945d99c386bb30414cc2638d1
wonje-choi <original.title.2000@gmail.com>
2026-03-06 22:34:30 +0900

    메시지

--- C/C_NOTE.c
+++ C/C_NOTE.c
@@ -50,16 +50,35 @@
 //     printf("You entered: %d\n", num);
 // }
 
+// #include <stdio.h>
+
+// int main (void) {
+//     int num;
+//     puts ("Enter a number:");
+//     scanf("%d", &num);
+//     if (num%2 == 0) {
+//         printf("짝수");
+//     } else {
+//         printf("홀수");
+//     }
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main (void) {
     int num;
-    puts ("Enter a number:");
-    scanf("%d", &num);
-    if (num%2 == 0) {
-        printf("짝수");
+    char name[10];
+    int select;
+    printf("name: ");
+    scanf("%s", name);
+    printf("name is [%s]. \n yes 1, no 2", name);
+    scanf("%d", &select);
+    if (select == 1) {  
+        printf("%s yes", name);
     } else {
-        printf("홀수");
+        printf("%s no", name);
     }
-    return 0;
+        return 0;
 }
+
